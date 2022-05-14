4d62aed9e62fb513f1820ad787e8df4381a88c32
Chai112 <sirdesignpie@gmail.com>
2022-05-14 18:09:49 +0700

    a

--- readme.md
+++ readme.md
@@ -2,14 +2,16 @@
 developed by Chaidhat Chaimongkol
 
 ### Server Installation
-This is usually configured for AWS EC2 and AWS RDS for backend.
-Ensure that the AWS RDS allows public connections for EC2 outside of the VPC and include all security groups. Make sure that when you are creating it that under additional information, specify a database name.
+This is usually configured for AWS EC2 Linux and AWS RDS for backend.
+Ensure that you go into security groups and edit inbound rules so that any ipv4 can be sent to it (add a new rule). Make sure that when you are creating it that under additional information, specify a database name.
 1. Get node and npm.
+2. Install dependencies
 ```
 $ npm install pm2 express mysql cors
 ```
-2. Retype the .PEM file directory in `ssh-server.sh` and `build-server.sh`
-3. Reconfigure the ssh/scp address in the same files.
-4. Reconfigure SQL connection to a MySQL server in `server/server.js`
-5. profit.
+3. Retype the .PEM file directory in `ssh-server.sh` and `build-server.sh`
+4. Reconfigure the ssh/scp address in the same files.
+5. Reconfigure SQL connection to a MySQL server in `server/server.js`
+6. Profit.
+
 Any difficulties contact chaidhatchaimongkol@gmail.com
--- server/server.js
+++ server/server.js
@@ -1,79 +1,5 @@
-console.log("add server")
-
 // install the libraries required
-const express = require('express')
-const mysql = require('mysql');
-
-// print that we are starting the server 
-console.log("starting server");
-
-// mysql home server
-// password is your normal F133 password
-// fcl03570@mzico.com (10/02/22)
-
-const CONNECTION_DETAILS = {
-  host     : 'chai-db.c2otfg86lzta.ap-southeast-1.rds.amazonaws.com',
-  database : 'my_db',
-  user     : 'admin',
-  port     : '3306',
-  password : 'nofpAz-zytke6-vycbyz',
-}
-
-var connection;
-
-// test connection
-// connect to MySQL server
-try {
-    _sqlConnect(() => {
-        // UNCOMMENT ON FIRST RUN
-        connection.query('CREATE TABLE IF NOT EXISTS Users ( userId int, username varchar(256), password varchar(256) ) ', function (error, results, fields) {
-            if (error) throw error;
-        });
-        connection.query('CREATE TABLE IF NOT EXISTS PostData ( views int, postDataId int, ratingScore int, ratingNumber int, data text ) ', function (error, results, fields) {
-            if (error) throw error;
-        });
-        connection.query('CREATE TABLE IF NOT EXISTS Posts ( postId int, postDataId int, userId int, comments text, date datetime ) ', function (error, results, fields) {
-            if (error) throw error;
-        });
-
-        /*
-        connection.query(
-            `DELETE FROM Posts`, function (err, results, fields) {
-        connection.query(
-            `DELETE FROM PostData`, function (err, results, fields) {
-            */
-
-        // retrieve all server data
-        // query server for post data
-        connection.query(
-            `SELECT * FROM PostData`, function (error, results, fields) {
-            if (error) throw error;     // error trapping
-            console.log(results);       // debug out the results
-            // query server for the posts
-            connection.query(
-                `SELECT * FROM Posts`, function (error, results, fields) {
-                if (error) throw error;     // error trapping
-                console.log(results);       // debug out the results
-                // query server for all the users
-                connection.query(
-                    `SELECT * FROM Users`, function (error, results, fields) {
-                    if (error) throw error;
-                    console.log(results);       // debug out the results
-                        _sqlDisconnect();
-                }); // end connection 3
-            });     // end connection 2
-        });         // end connection 1
-
-        /*
-        });
-        });
-        */
-
-        console.log("TEST: PASSED   connected to server ");
-    });
-} catch {
-    console.log("TEST: FAILED   failed to connect to database");
-}
+const express = require('express');
 
 // start the application server via Express library
 const app = express()   // setup Express
@@ -83,38 +9,70 @@ const port = 3000       // specify the port required
 const cors = require('cors');
 app.use(cors());
 app.use(express.json({limit: '10gb'}));
+
+const databaseModule = require('./database');
+const db = new databaseModule.Database();
+
+// print that we are starting the server 
+console.log("starting server");
+
+// mysql home server
+// password is your normal F133 password
+// fcl03570@mzico.com (10/02/22)
+
+
+test();
+async function test() {
+    // test connection
+    // connect to MySQL server
+    //try {
+        //await db.query("hey");
+        await db.dropTable("Users");
+        await db.assertTable("Users",
+            [
+                {
+                "name": "username",
+                "type": "varchar(50)"
+                },
+                {
+                "name": "password",
+                "type": "varchar(32)"
+                }
+        ]);
+        await db.insertIntoTable("Users",
+        {
+            username: "cat",
+            password: "bat"
+        });
+        await db.insertIntoTable("Users",
+        {
+            username: "cat",
+            password: "mat"
+        });
+        let data = await db.selectTable("Users");
+        console.log(data);
+        db.updateTable("Users", 
+            // where...
+            {username: "cat"},
+            // set entry to...
+            {
+                username: "bat",
+                password: "nat",
+            }
+        );
+        data = await db.selectTable("Users");
+        console.log(data);
+        await db.dropTable("Users");
+        console.log("TEST: PASSED   successfully connected to database");
+        /*
+    } catch {
+        console.log("TEST: FAILED   failed to connect to database");
+    }
+    */
+}
+
 //app.use(express.urlencoded({limit: '10gb'}));
 
-// connect to the MySQL server
-var connected = false;
-
-function _sqlConnect(callback) {
-    if (connected)  // if already connected
-        return;
-    // handshake
-    connection = mysql.createConnection(CONNECTION_DETAILS);
-    // then perform connection
-    connection.connect((err) => {
-        if (err) {
-            console.log(err.message);
-            throw err;
-        }
-        connected = true;
-        console.log("SQL:   Connected");
-        callback();
-    });
-}
-
-// disconnect from the MySQL server
-function _sqlDisconnect() {
-    if (!connected)  // if already disconnected
-        return;
-
-    // terminate connection to server
-    connection.end();
-    connected = false;
-    console.log("SQL:   Disconnected");
-}
 
 // this is called everytime a request is GET'ted at localhost
 app.get('/', function (request, response) { // call a function where request and response are arguments
